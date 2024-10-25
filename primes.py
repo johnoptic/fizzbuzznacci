b59b67ae1d1fb14c840d9f74002bccf3b44fb51c
John <Johnoptic@proton.me>
2024-10-25 19:11:55 +0100

    prime numbers loop

--- primes.py
+++ primes.py
@@ -1,17 +1,18 @@
-# Run a loop that checks for primes (can only be divided by 1 and itself)
-# Print to console
+prime_list = []
 
 def find_primes(input_num):
-    i = 1
-    while(i <= input_num):
-        if i % i == 0: # Prints every number because it is dividing by itself and checking for remainder
-            print(i)
-        i+=1
+    for num in range(2, input_num + 1): # First variable running through 2 upto the input value. 
+        is_prime = True  # Assume the number is prime until after condition check
+        for factors in range(2, int(num**0.5) + 1):  # takes num and runs a loop for secondary variable factor, between a range of 2 and nums square root.
+            if num % factors == 0:  # condition check to see if the num is divisible by any of the factors. If it is only div by itself and 1.
+                is_prime = False
+                print(num)
+                break
+        if is_prime:
+            i_am_prime = f"{num} is a PRIME!"
+            print(i_am_prime)
+            prime_list.append(num)
 
-find_primes(20)
+find_primes(100)
 
-# checking each integer 1, 2, 3, 4, 5, 6, 7, 8 etc
-
-# pick non prime 4
-# is 4 divided by 1 = 4. yes.     is 4 divided by 4. yes. 
-# just asking these two questions alone is not enough.
+print(prime_list) # prints just the primes
